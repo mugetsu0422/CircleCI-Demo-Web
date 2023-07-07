0c57962aee078fab1ac7c4c525cf55f574e6ae4e
Samuel <20127610@student.hcmus.edu.vn>
2023-07-07 21:48:05 +0700

    add function calculate streak

--- utils/helper.js
+++ utils/helper.js
@@ -26,21 +26,3 @@ export const getVisiblePage = (totalPages, visiblePages, curPage) => {
     }
     return pages
 }
-
-export const calcStreak = (timestamp, streakinfo) => {
-    let streak = streakinfo.streak
-    let lastdaylogin = streakinfo.lastlogindate
-    
-    const lastdayloginUTC = new Date(lastdaylogin).toISOString().slice(0,10)
-    const timestampUTC = timestamp.toISOString().slice(0, 10)
-    const yesterday = new Date((timestamp.getTime() - 86400000)).toISOString().slice(0, 10)
-
-    if (lastdayloginUTC === yesterday && timestampUTC !== yesterday) {
-        streak = streak + 1
-    }else if(timestampUTC === lastdayloginUTC){
-        streak
-    }else {
-        streak = 1
-    }
-    return streak
-}
